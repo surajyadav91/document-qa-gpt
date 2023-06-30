4f685a1205254a8fe9120bde6a116e6a0b3cbeae
surajyadav91 <syadav897@Yadavs-MacBook-Pro.local>
2023-06-30 21:53:00 +0530

    config changes

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ config = {  'chunks_path': "chunks.pkl",
             'chat_gpt_model':'gpt-3.5-turbo',
             'chunk_size': 2000,
             'overlap_chunk_size': 400,
-            'file_path': './SpaceX_NASA_CRS-5_PressKit.pdf'
+            'file_path': './SpaceX_NASA_CRS-5_PressKit.pdf',
+            'top_k': 5,
 }
--- retriever.py
+++ retriever.py
@@ -10,12 +10,13 @@ class Retriever:
         self.hf_dataset = load_from_disk(config['hf_dataset'])
         self.hf_dataset.load_faiss_index('embedding',config['faiss_index_path'])
         self.embedding_model = config['embedding_model']
+        self.top_k = config['top_k']
 
     def retrieve(self, question):
         question_embedding = openai.Embedding.create(input=question,model=self.embedding_model)['data'][0]['embedding']
         question_embedding = np.array(question_embedding)
 
-        scores,samples = self.hf_dataset.get_nearest_examples('embedding',question_embedding,k=5)
+        scores,samples = self.hf_dataset.get_nearest_examples('embedding',question_embedding,k=self.top_k)
 
         samples_df = pd.DataFrame.from_dict(samples)
         samples_df['scores'] = scores
