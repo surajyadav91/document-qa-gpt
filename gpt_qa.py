b1c93a786772094f3030023b1a0474ef9fc42eda
surajyadav91 <syadav897@Yadavs-MacBook-Pro.local>
2023-11-15 04:45:52 +0530

    code changes

--- gpt_qa.py
+++ gpt_qa.py
@@ -30,10 +30,3 @@ class GPT_QA:
         return response["choices"][0]["message"]["content"]
 
 
-# if __name__ == "__main__":
-#     # test the function
-#     question = "Do I need to bond when the customer has solar power? "
-#     context = "If these conditions are not met, notify your supervisor and customer that a valid bonding option is not available. 1.1.3 All technicians must comply with local electrical codes, as some juri ictions do not permit certain bonding methods. Figure 6-2 Drop Bonding to Metal Frame / I-Beam of the Structure 7 Solar Power Bonding 7.1 Per NEC Article 690.43 (Solar Photovoltaic (PV) Systems - Equipment Grounding and Bonding), the solar power system is required to be grounded and the grounded conductor is required to be bonded to the grounded ele rode conductor of the power system."
-#     reader = GPTTurboReader()
-#     response = reader.read(question=question, passage=context)
-#     print(response)
